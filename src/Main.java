fb9288a98bce433ac3fe1e56c285c9e99bfafdfc
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-10-18 14:47:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] mat = new int[2][3];
+        //DATA
+        int[][] data1 = {
+                {1,1,2},
+                {2,2,2},
+                {4,3,3}
+        };
 
-        // Matriz M(y,x)
-        // Eixo Y vertical, linhas
-        // Eixo X horizontal, colunas
+        int[][] data2 = {
+                {1,2,2},
+                {2,2,2},
+                {4,4,4}
+        };
 
-        mat[0][2] = 1;
-        print(mat);
+        // result
+        int[][] res = new int[3][3];
+
+        for (int i = 0; i < res.length; i++) {
+            for (int o = 0; o < res[0].length; o++) {
+                int[] colors = {data1[i][o], data2[i][o]};
+                res[i][o] = calcColor(colors);
+            }
+        }
+
+        print(res);
     }
 
     static void print(int[][] mat) {
@@ -21,5 +37,21 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    static int calcColor(int[] colors) {
+        // 1 - blue
+        // 2 - light blue
+        // 3 - green
+        // 4 - palha
+        // 5 - yellow
+        // 6 - orange
+        // 7 - red
+
+        int plus = 0;
+        for (int color : colors) {
+            plus = plus + color;
+        }
+        return plus / colors.length;
     }
 }
