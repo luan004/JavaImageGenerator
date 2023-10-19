6a781928222cbd3b33aac9cd5b0023c12d38b228
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2023-10-19 14:35:22 -0300

    19-10

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,10 @@ public class Main {
         String local = "src/data/3x3";
 
         //DATA
-        int[] data = imageProcessor(size, local);
+        ArrayList<int[][]> data = imageProcessor(size, local);
+
+        System.out.println((-16764837 + -1237980) / 2);
+        System.out.println(getColor(-9001408));
 
         int[][] data1 = {
                 {1,1,2},
@@ -67,16 +70,17 @@ public class Main {
         return plus / colors.length;
     }
 
-    static int[] imageProcessor(int size, String local) {
+    static ArrayList<int[][]> imageProcessor(int size, String local) {
         // FOLDER PATH
         File path = new File(System.getProperty("user.dir") + File.separator + local);
 
-        ArrayList<String[][]> processedImages = new ArrayList<>();
+        ArrayList<int[][]> processedImages = new ArrayList<>();
 
         if (path.isDirectory()) {
             File[] files = path.listFiles();
 
             // FOR EACH FILE
+            assert files != null;
             for (File file: files) {
                 // IS A JPG IMAGE?
                 if (file.isFile() && file.getName().toLowerCase().endsWith(".jpg")) {
@@ -89,19 +93,17 @@ public class Main {
                         if (image != null && image.getWidth() == size && image.getHeight() == size) {
                             // O ARQUIVO É VÁLIDO
 
-                            String[][] processedImage = new String[size][size];
+                            int[][] processedImage = new int[size][size];
 
                             for (int y = 0; y < size; y++) {
                                 for (int x = 0; x < size; x++) {
-                                    // Obtém a cor em RGB como uma string
-                                    String rgbString = getColor(image.getRGB(x, y));
-
                                     // Armazena a cor no array
-                                    processedImage[y][x] = rgbString;
+                                    processedImage[y][x] = image.getRGB(x, y);
                                 }
                             }
 
                             processedImages.add(processedImage);
+                            print(processedImage);
 
                         } else {
                             System.err.println("A imagem '" + file.getName() + "' está corrompida ou não tem tamanho " + size + "x" + size);
@@ -117,7 +119,7 @@ public class Main {
             System.err.println("Diretório não encontrado: " + path);
         }
 
-        return new int[]{0};
+        return processedImages;
     }
 
     static String getColor(int rgb) {
@@ -126,5 +128,25 @@ public class Main {
         int blue = rgb & 0xFF;
 
         return red + "," + green + "," + blue;
+    }
+
+    static int colorMix(int[] pixelsColor) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+
+        for (int rgb: pixelsColor) {
+            r = r + ((rgb >> 16) & 0xFF);
+            g = g + ((rgb >> 8) & 0xFF);
+            b = b + (rgb & 0xFF);
+        }
+
+        int l = pixelsColor.length;
+
+        r = r/l;
+        g = g/l;
+        b = b/l;
+
+        return (r << 16) | (g << 8) | b;
     }
 }
